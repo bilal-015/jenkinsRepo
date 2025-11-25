d5266bb5c4d2a4873ca13b94762958f6c997df95
bilal-015 <bk212986@gmail.com>
2025-11-25 12:10:39 +0500

    second commit

--- test.java
+++ test.java
@@ -1,5 +1,5 @@
 // make a simple hello world program
-public class HelloWorld {
+public class test {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
